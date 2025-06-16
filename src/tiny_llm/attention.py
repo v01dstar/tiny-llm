272b5f009150a39397788b366814c64c4ce870cd
Yang Zhang <yang.zhang@pingcap.com>
2025-06-15 21:48:25 -0700

    Finish week 1 day 1
    Signed-off-by: Yang Zhang <yang.zhang@pingcap.com>

--- src/tiny_llm/attention.py
+++ src/tiny_llm/attention.py
@@ -1,7 +1,6 @@
 import mlx.core as mx
 from .basics import softmax, linear
 
-
 def scaled_dot_product_attention_simple(
     query: mx.array,
     key: mx.array,
@@ -9,7 +8,21 @@ def scaled_dot_product_attention_simple(
     scale: float | None = None,
     mask: mx.array | None = None,
 ) -> mx.array:
-    pass
+    # query, key, value: (B, H, L, D)  and (B, H, S, D) for K, V
+    L = query.shape[-2]
+    S = key.shape[-2]
+    D = query.shape[-1]
+    scale_factor = 1.0 / mx.sqrt(D) if scale is None else scale
+    attention_bias = mx.zeros(query.shape[:-2] + (L, S), dtype=query.dtype)
+    if mask is not None:
+        attention_bias = mask
+    # B, H, L, D @ B, H, S, D -> B, H, L, S
+    attention_scores = query @ key.swapaxes(-1, -2) * scale_factor
+    attention_scores = attention_scores + attention_bias
+    attention_scores = softmax(attention_scores, axis=-1)
+    # B, H, L, S @ B, H, S, D -> B, H, L, D
+    output = attention_scores @ value
+    return output
 
 
 class SimpleMultiHeadAttention:
@@ -22,7 +35,13 @@ class SimpleMultiHeadAttention:
         wv: mx.array,
         wo: mx.array,
     ):
-        pass
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
+        self.E = hidden_size # hidden size
+        self.H = num_heads # number of attention heads
+        self.D = self.E // self.H # dimension of each attention head
 
     def __call__(
         self,
@@ -31,7 +50,22 @@ class SimpleMultiHeadAttention:
         value: mx.array,
         mask: mx.array | None = None,
     ) -> mx.array:
-        pass
+        # query, key, value: (B, L, E)  and (B, S, E) for K, V
+        # Q: (B, L, E) @ (E, H*D) -> (B, L, H*D)
+        shape = query.shape[:-1] + (self.H, self.D)
+        Q = linear(query, self.wq).reshape(shape).swapaxes(-2, -3)
+        K = linear(key, self.wk).reshape(shape).swapaxes(-2, -3)
+        V = linear(value, self.wv).reshape(shape).swapaxes(-2, -3)
+        # Reshape to (B, L, H, D) and swap axes to (B, H, L, D)
+
+        # Attention scores: (B, H, L, D) @ (B, H, D, S) -> (B, H, L, S)
+        attention_scores = scaled_dot_product_attention_simple(Q, K, V, mask=mask)
+
+        # Reshape back to (B, L, H*D)
+        attention_scores = attention_scores.swapaxes(-2, -3)
+        attention_scores = attention_scores.reshape(query.shape[:-1] + (self.E,))
+        attention_scores = linear(attention_scores, self.wo)
+        return attention_scores
 
 
 def causal_mask(L: int, S: int, dtype: mx.Dtype) -> mx.array:
--- src/tiny_llm/basics.py
+++ src/tiny_llm/basics.py
@@ -7,12 +7,17 @@ def softmax(x: mx.array, axis: int) -> mx.array:
     return mx.softmax(x, axis=axis)
 
 
+# x is N.. x I
+# w is O x I
+# bias is O
+# I may not be 1 dimensional
+
 def linear(
     x: mx.array,
     w: mx.array,
     bias: mx.array | None = None,
 ) -> mx.array:
-    pass
+    return x @ w.T + (bias if bias is not None else 0)
 
 
 def silu(x: mx.array) -> mx.array:
